39e3af6a2cf197d8073920354cf5f00a539e8c10
Ivan Radko <mr.radkoivan@gmail.com>
2023-08-12 18:24:18 +0300

    added validation for phone and email

--- schemas/bodySchema.js
+++ schemas/bodySchema.js
@@ -2,8 +2,18 @@ const Joi = require("joi");
 
 const bodySchema = Joi.object({
   name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net"] },
+    })
+    .required(),
+  phone: Joi.string()
+    .regex(/^\(\d{3}\)\s\d{3}-\d{4}/)
+    .messages({
+      "string.pattern.base": "Phone number format must be: (111) 111-1111",
+    })
+    .required(),
 });
 
 module.exports = {
